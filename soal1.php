4fc18496ea8578c41d89ca1c949fc1b4f276eec3
rakarahadyan <rakarahadyan177@gmail.com>
2025-09-01 21:28:04 +0700

    soal a nomor 1

---
+++ soal1.php
@@ -0,0 +1,17 @@
+<?php
+
+$jml = $_GET['jml'];
+echo "<table border=1>\n";
+for ($a = $jml; $a > 0; $a--) {
+    $totalRow = 0;
+    for ($b = $a; $b > 0; $b--) {
+        $totalRow += $b;
+    }
+    echo "<tr><td colspan='$jml' >Total : $totalRow</td></tr>";
+    echo "<tr>\n";
+    for ($b = $a; $b > 0; $b--) {
+        echo "<td>$b</td>";
+    }
+    echo "</tr>\n";
+}
+echo "</table>";
